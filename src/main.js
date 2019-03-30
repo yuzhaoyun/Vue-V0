db5fb82927325e6c4f1f08d9f76c2ec3abef80d5
yuzhaoyun <1901272896@qq.com>
2019-03-31 00:00:45 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// 入口文件
+import Vue from 'vue';
+import App from './App.vue';
+// 导入全部的mint-UIz组件
+// import MintUI from 'mint-ui';
+// import 'mint-ui/lib/style.css';
+// Vue.use(MintUI);
+// 按需导入mint-ui
+import {Header} from 'mint-ui';
+Vue.component(Header.name,Header)
+// 引入mui的样式
+import './lib/mui/css/mui.min.css';
+
+var vm=new Vue({
+    el:"#app",
+    render:c=>c(App)
+})
---
+++ src/router.js
@@ -0,0 +1,11 @@
+// 1. 导入vue-router包
+import VueRouter from 'vue-router'
+
+// 3. 创建路由对象
+var router = new VueRouter({
+    routes: [
+        
+    ]
+})
+
+export default router;
